4d01a25d35b7eb22513b7f3f54973a6e61abec0f
mploetz <mploetz@indiana.edu>
2017-10-23 23:50:47 -0400

    update

--- Assignment3/A3Code/board.py
+++ Assignment3/A3Code/board.py
@@ -7,40 +7,102 @@ Created on Sat Sep 24 10:34:05 2016
 import search
 import numpy as np
 class Board:
-    def __init__(self, orig=None, hash=None):
-        # copy all
-        if (orig):
-            self.board = [list(col) for col in orig.board]
-            self.numMoves = orig.numMoves
-            self.lastMove = orig.lastMove
-            # try copying input board
-            try:
-                self.won = orig.won
-                self.threats0 = orig.threats0.copy()
-                self.threats1 = orig.threats1.copy()
-                self.heur = orig.heur
-                self.dirty = orig.dirty
-            # otherwise set missing fields as defaults
-            except:
-                self.won = False
-                self.threats0 = set()
-                self.threats1 = set()
-                self.heur = 0
-                self.dirty = False
-            return
-        
+    WIDTH = 7
+    HEIGHT = 6
+    def __init__(self):
+        self.board = self.empty_board()
+        self.won = False
+        self.moves = []
+        self.lastMove = None
+        self.numMoves = 0
+    
+    # creates an empty board
+    def empty_board(self):
+        self.board = []
+        for row in range(self.HEIGHT):
+            self.board.append([])
+            for col in range(self.WIDTH):
+                self.board[row].append('_')
+
     def generate_moves(self):
         # TODO
-        
+        children = []
+        for i in range(self.WIDTH):
+            if len(self.board[i]) < self.HEIGHT:
+                child = self.board.deepcopy()
+                child.make_move(i)
+                children.append(i)
+        return children
+
+    # makes a move
+    # Does not check if the move is valid
     def make_move(self,c):
         # TODO
+        # whos turn moving
+        piece = self.numMoves % 2
+        # keep track of that move
+        self.lastMove = (piece, c)
+        # update moves
+        self.numMoves += 1
+        # keep track of all moves
+        self.moves.append(self.lastMove)
+        # put the piece on the board at given move(c)
+        self.board[c].append(piece)
             
     def unmake_last_move(self):
         # TODO
+        pass
                 
     def last_move_won(self):
         # TODO
-    
+        horscore = 0
+        verscore = 0
+        rightdiag = 0
+        leftdiag = 0
+        phor = 0
+        pver = 0
+        pleft = 0
+        pright = 0
+        for row in range(height):
+                horscore = 0
+            for col in range(width):
+            verscore = 0
+            if board[row][col] == turn.symbol:
+                horscore += 1
+            else:
+                horscore = 0
+            for row2 in range(height):
+                if board[row2][col] == turn.symbol:
+                    verscore += 1
+                else:
+                    verscore = 0
+        # checks \
+        for i in range(3):
+                for j in range(4):
+                prev = self.board[i][j]
+            for k in range(1, 4):
+                if self.board[i+k][j+k] != 0 and self.board[i+k][j+k] == prev):
+                    rightDiag += 1
+                if (rightDiag == 4):
+                    return True
+                else:
+                    break
+                rightDiag = 1
+                prev = 0
+            return False
+        
+        for row in range(3, 6):
+            for col in range(4):
+                prev = self.board[row][col]
+            for k in range(1,4):
+                if self.board[row-k][col+k] != 0 and self.board[row-k][col+k] == prev:
+                leftdiag += 1
+                if leftdiag == 4:
+                            return self.board[row-k][col+k]
+            else:
+                break
+            leftdiag = 1
+            prev = 0
+
     def __str__(self):
-        return str(np.matrix(board))
-    
+        return str(np.matrix(self.board))   
--- Assignment3/A3Code/search.py
+++ Assignment3/A3Code/search.py
@@ -9,16 +9,15 @@ import numpy as np
 
 def minimax(b,depth):
     # TODO    
-    
-
+    pass
 
 def minimax_root(b,depth):
     # TODO 
-
-
+    pass
 
 def alphabeta_minimax(b,depth,a,be):
     # TODO
-    
+    pass
 def minimax_root_alphabeta(b, depth):
     # TODO
+    pass
