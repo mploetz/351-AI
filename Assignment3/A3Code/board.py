02265ef2b2866b2d7d80fde63c40f63f769b0137
mploetz <mploetz@indiana.edu>
2017-10-24 04:18:50 -0400

    update

--- Assignment3/A3Code/board.py
+++ Assignment3/A3Code/board.py
@@ -12,9 +12,10 @@ class Board:
     def __init__(self):
         self.board = self.empty_board()
         self.won = False
-        self.moves = []
+        self.parent = self.empty_board()
         self.lastMove = None
         self.numMoves = 0
+        self.turn = 0
     
     # creates an empty board
     def empty_board(self):
@@ -63,42 +64,42 @@ class Board:
         pver = 0
         pleft = 0
         pright = 0
-        for row in range(height):
-                horscore = 0
-            for col in range(width):
-            verscore = 0
-            if board[row][col] == turn.symbol:
-                horscore += 1
-            else:
-                horscore = 0
-            for row2 in range(height):
-                if board[row2][col] == turn.symbol:
-                    verscore += 1
+        for row in range(self.HEIGHT):
+            horscore = 0
+            for col in range(self.WIDTH):
+                verscore = 0
+                if self.board[row][col] == self.turn:
+                    horscore += 1
                 else:
-                    verscore = 0
+                    horscore = 0
+                for row2 in range(self.HEIGHT):
+                    if self.board[row2][col] == self.turn:
+                        verscore += 1
+                    else:
+                        verscore = 0
         # checks \
         for i in range(3):
-                for j in range(4):
+            for j in range(4):
                 prev = self.board[i][j]
-            for k in range(1, 4):
-                if self.board[i+k][j+k] != 0 and self.board[i+k][j+k] == prev):
-                    rightDiag += 1
-                if (rightDiag == 4):
-                    return True
-                else:
-                    break
-                rightDiag = 1
-                prev = 0
-            return False
+                for k in range(1, 4):
+                    if self.board[i+k][j+k] != 0 and self.board[i+k][j+k] == prev:
+                        rightDiag += 1
+                    if (rightDiag == 4):
+                        return True
+                    else:
+                        break
+                    rightDiag = 1
+                    prev = 0
+        return False
         
         for row in range(3, 6):
             for col in range(4):
                 prev = self.board[row][col]
             for k in range(1,4):
                 if self.board[row-k][col+k] != 0 and self.board[row-k][col+k] == prev:
-                leftdiag += 1
+                    leftdiag += 1
                 if leftdiag == 4:
-                            return self.board[row-k][col+k]
+                    return self.board[row-k][col+k]
             else:
                 break
             leftdiag = 1
